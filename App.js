ecb6989dbed681d4ca8b79300a9cec0f57effc93
id-aaryan <am5020@imperial.ac.uk>
2022-06-09 16:39:44 +0100

    revert

--- App.js
+++ App.js
@@ -2,16 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-import { AuthScreen } from './screens';
-
 export default function App() {
   return (
     <View style={styles.container}>
-      <AuthScreen />
+      <Text>Change App.js!</Text>
       <StatusBar style="auto" />
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
